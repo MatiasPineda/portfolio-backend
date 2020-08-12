d3b1de2c9f5b96c5368c64f3c690c02a871031ea
MatiasPineda <matias.pinedav@gmail.com>
2020-08-12 13:42:37 -0400

    setup

---
+++ portfolio_project/settings.py
@@ -0,0 +1,116 @@
+from os import path
+
+import environ
+
+ROOT = environ.Path(__file__).path('../' * 2)
+ENV = environ.Env(DJANGO_DEBUG=(bool, False), )
+if path.isfile(ROOT('.env')):
+    environ.Env.read_env(ROOT('.env'))
+
+SITE_ROOT = ROOT()
+
+DEBUG = ENV('DJANGO_DEBUG')
+SECRET_KEY = ENV('DJANGO_SECRET_KEY')
+
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'whitenoise.runserver_nostatic',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Local
+    # 'users',
+    # 'farmacias_turno_app',
+    # 'portfolio_app',
+    # 'blog_app',
+    # 'flashcards_app',
+
+    # 3rd party
+    'rest_framework',
+    'corsheaders',
+    # 'subdomains'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    # 'subdomains.middleware.SubdomainURLRoutingMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'portfolio_project.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [ROOT('templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            'debug': DEBUG,
+        },
+    },
+]
+
+WSGI_APPLICATION = 'portfolio_project.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {'default': ENV.db()}
+if ENV('CI', default=False):
+    DATABASES['default']['TEST'] = ENV.db()
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.1/topics/i18n/
+
+LANGUAGE_CODE = 'es'
+TIME_ZONE = 'America/Santiago'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_ROOT = ROOT('staticfiles')
+STATICFILES_DIRS = ['static']
+STATIC_URL = '/static/'
---
+++ portfolio_project/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.Home.as_view(), name='home'),
+    # path('api/v1.0/', include('farmacias_turno_app.urls')),
+    # path('api/v1.0/', include('portfolio_app.urls')),
+]
